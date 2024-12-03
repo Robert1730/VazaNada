aa2951058f035736129b17bab30fb07dc7b8054c
Robert1730 <robert.souza@sptech.school>
2024-12-03 19:19:22 -0300

    Adicionando validação de usuário

--- site/public/global.js
+++ site/public/global.js
@@ -1,18 +1,18 @@
 function validarSessao() {
 
-    var id = sessionStorage.ID_USUARIO
+    var permissao = sessionStorage.PERMISSAO
+    var nome = sessionStorage.nome
+    var spanNivel = document.getElementById("nivelSuporte")
+    var spanNome = document.getElementById("nomeSuporte")
+
+    spanNivel.innerHTML = permissao
+    spanNome.innerHTML = nome
     
-    if (id = 1) {
-        var navHome = document.getElementById("homeNav")
-        navHome.innerHTML = `BobIA`
+    if (permissao == 'N3') {
+        var divBob = document.getElementById("divBob")
+        divBob.style.display = "none";
 
     }
-}
-
-function limparSessao() {
-    sessionStorage.clear();
-    window.location = "../index.html";
-
 }
 
 validarSessao()
