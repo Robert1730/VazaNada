b11fd7a0aa65abb584fcff5854f9fd69a9773e3d
KaioCassiano <kaio.barbosa@sptech.school>
2024-12-04 09:55:23 -0300

    Atualização

--- site/public/global.js
+++ site/public/global.js
@@ -1,18 +1,18 @@
-function validarSessao() {
+// function validarSessao() {
 
-    var permissao = sessionStorage.PERMISSAO
-    var nome = sessionStorage.NOME_USUARIO
-    var spanNivel = document.getElementById("nivelSuporte")
-    var spanNome = document.getElementById("nomeSuporte")
+//     var permissao = sessionStorage.PERMISSAO
+//     var nome = sessionStorage.NOME_USUARIO
+//     var spanNivel = document.getElementById("nivelSuporte")
+//     var spanNome = document.getElementById("nomeSuporte")
 
-    spanNivel.innerHTML = permissao
-    spanNome.innerHTML = nome
+//     spanNivel.innerHTML = permissao
+//     spanNome.innerHTML = nome
     
-    if (permissao == 'N3') {
-        var divBob = document.getElementById("divBob")
-        divBob.style.display = "none";
+//     if (permissao == 'N3') {
+//         var divBob = document.getElementById("divBob")
+//         divBob.style.display = "block";
 
-    }
-}
+//     }
+// }
 
-validarSessao()
+// validarSessao()
